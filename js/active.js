3ab932b430e70f206f4fc6986724a0baf825985e
Akash Paul <iakashpaul@gmail.com>
2017-07-06 02:41:38 +0530

    mekey rebase

--- js/active.js
+++ js/active.js
@@ -1,111 +1,109 @@
 /*-----------------------firebase storage-----------------------*/
-function queryDatabase(token) {
+function queryDatabase(token){
     firebase.database().ref().once('value').then(function(snapshot) {
         var postObject = snapshot.val();
 
         var lib = " ";
-        for (key in postObject) {
-            var l = 1;
+        for (key in postObject){
+        var l=1;
 
-            if (typeof postObject[key] == 'object') {
+            if(typeof postObject[key] == 'object'){
                 continue;
             }
 
-            lib += '<div class="box box' + l + ' wow fadeInUp clust">';
-            lib += '<div class="reposDiv">';
-
-            lib += '<a href="' + postObject[key] + '" download><i class="fa fa-download fa-2x" aria-hidden="true"></i></a>';
-            lib += '</div><figure class="newFigure"> <img src="' + postObject[key] + '" class="gifPic"></figure><strong></strong>';
-
-            lib += '</div>';
-            ++l;
+        lib +='<div class="box box'+l+' wow fadeInUp clust">';
+        lib +='<div class="reposDiv">';
+        
+        lib +='<a href="'+postObject[key]+'" download><i class="fa fa-download fa-2x" aria-hidden="true"></i></a>';
+        lib +='</div><figure class="newFigure"> <img src="'+postObject[key]+'" class="gifPic"></figure><strong></strong>';
+        
+        lib +='</div>';
+        ++l;
         }
 
         $(".library").html(lib);
     })
 }
 
-var onceBool;
 
 
-jQuery(document).ready(function() {
-    onceBool = true;
-    $('.login-form .new-animake').off('click').on('click', function() {
+
+jQuery(document).ready(function(){
+    $('.login-form .new-animake').off('click').on('click',function(){
         $('.login-form').hide();
         $('.reg-form').show();
     });
-
-    $('.reg-form .already-reg').off('click').on('click', function() {
+    
+    $('.reg-form .already-reg').off('click').on('click',function(){
         $('.reg-form').hide();
         $('.login-form').show();
     });
 
-    $('.clustering .box, .tp').off('click').on('click', function(e) {
-        if (!$(e.target).is('.fa')) {
-            window.location.href = $(this).attr('data-page') + ".html";
+	/*
+    $('.clustering .box, .tp').off('click').on('click', function(e){
+        if(!$(e.target).is('.fa')) {
+            window.location.href= $(this).attr('data-page') + ".html";
         }
     });
+	*/
 
-    $('.openModal').off('click').on('click', function(e) {
+    $('.openModal').off('click').on('click', function(e){
         e.preventDefault();
         $('#js-modal').show();
     })
+    
 
-
-    /*--------------------------firebase storage--------------------*/
+/*--------------------------firebase storage--------------------*/
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
             // User is signed in.
             var token = firebase.auth().currentUser.uid;
             queryDatabase(token);
-
-            // closeLoginModal();
-
-            $('#myModal').hide();
-            $('.reg').hide();
-            $('.login-form').hide();
-            $('.reg-form').hide();
-
-
+            
         }
     });
 
-    /*---------------firebase DB zodiacCont--------------------*/
+/*---------------firebase DB zodiacCont--------------------*/
 
-    // firebase.database().ref("/fire_pic/").once('value').then(function(snapshot) {
-    //     var fireObject = snapshot.val();
-    //     var storageRef = firebase.storage().ref();
-    //     var gsReference = firebase.storage().refFromURL('gs://animake-672fc.appspot.com');
-    //     var gifs = {};
-    //     var anims = {};
-    // // console.log("fireObject = ", fireObject);
-    //     for (key in fireObject) {
-    //         var obj = fireObject[key].split("-");
-    // // console.log("obj = ", obj);
-    //         var gif = 'gifsFires/'+obj[0];
-    // // console.log("gif = ", gif);
-    //         var anim = 'Animake/'+obj[1];
-    // // console.log("anim = ", anim);
-    //         var spaceRef = storageRef.child(gif);
-    // // console.log("spaceRef = ", spaceRef);
-    // // console.log("storageRef.child(",gif,").getDownloadURL() = ", storageRef.child(gif).getDownloadURL());
-    //         storageRef.child(gif).getDownloadURL().then(function(url) {
-    // // console.log("gif url = ", url);
-    //             var num = url.match(/F\d+/);
-    // // console.log("gif num = ", num);
-    //             gifs[num] = url;
-    //             sessionStorage.setItem('satorage_gifs', JSON.stringify(gifs));
-    //         }).catch(function(error) {});
-    //
-    //         storageRef.child(anim).getDownloadURL().then(function(url) {
-    // // console.log("anim url = ", anim);
-    //             var num = url.match(/F\d+/);
-    // // console.log("anim num = ", num);
-    //             anims[num] = url;
-    //             sessionStorage.setItem('satorage_anims', JSON.stringify(anims));
-    //         }).catch(function(error) {})
-    //     }
-    // });
-    // console.log("sessionStorage.getItem('satorage_gifs') = ", sessionStorage.getItem('satorage_gifs'));
-    // console.log("sessionStorage.getItem('satorage_anims') = ", sessionStorage.getItem('satorage_anims'));
+    firebase.database().ref("/fire_pic/").once('value').then(function(snapshot) {
+        var fireObject = snapshot.val();
+        var storageRef = firebase.storage().ref();
+        var gsReference = firebase.storage().refFromURL('gs://animake-672fc.appspot.com');
+        var gifs = {};
+        var anims = {};
+    console.log("fireObject = ", fireObject);
+        for (key in fireObject) {
+            var obj = fireObject[key].split("-");
+    console.log("obj = ", obj);
+            var gif = 'gifsFires/'+obj[0];
+    console.log("gif = ", gif);
+            var anim = 'Animake/'+obj[1];
+    console.log("anim = ", anim);
+            var spaceRef = storageRef.child(gif);
+    console.log("spaceRef = ", spaceRef);
+    console.log("storageRef.child(",gif,").getDownloadURL() = ", storageRef.child(gif).getDownloadURL());
+            storageRef.child(gif).getDownloadURL().then(function(url) {
+    console.log("gif url = ", url);
+                var num = url.match(/F\d+/);
+    console.log("gif num = ", num);
+                gifs[num] = url;
+                sessionStorage.setItem('satorage_gifs', JSON.stringify(gifs));
+            }).catch(function(error) {});
+    
+            storageRef.child(anim).getDownloadURL().then(function(url) {
+    console.log("anim url = ", anim);
+                var num = url.match(/F\d+/);
+    console.log("anim num = ", num);
+                anims[num] = url;
+                sessionStorage.setItem('satorage_anims', JSON.stringify(anims));
+            }).catch(function(error) {})
+        }
+    });
+    console.log("sessionStorage.getItem('satorage_gifs') = ", sessionStorage.getItem('satorage_gifs'));
+    console.log("sessionStorage.getItem('satorage_anims') = ", sessionStorage.getItem('satorage_anims'));
 })
+
+
+
+
+
--- js/repository.js
+++ js/repository.js
@@ -1,131 +1,119 @@
-var resultsPerPage = 12;
-var pages = 0;
 
-var animationsArray = []
-$.blockUI();
-firebase.database().ref("animations").orderByChild("name").once("value", function(ss) {
-    var allAnimations = ss.val();
-    Object.keys(allAnimations).forEach(function(animKey) {
-        animationsArray.push(allAnimations[animKey]);
-    });
+jQuery(document).ready(function(){
 
-    getVideos(1);
-});
+/*---------firebase storage zodiacCont--------------*/
 
-function getVideos(page, th) {
-    $.blockUI();
-    if (th != undefined) {
-        $(th).parent().parent().find(".active").toggleClass("active");
-        $(th).parent().toggleClass("active");
-    }
-    var offset = (page - 1) * resultsPerPage;
-    var blocks = '';
-    var k = 1;
-    var completed = 1;
-    var data = animationsArray.slice(offset, (page * resultsPerPage));
-    if (!data.length) {
-        $.unblockUI();
-    } else {
-        data.forEach(function(anim) {
+if (sessionStorage.getItem("satorage_gifs") && sessionStorage.getItem("satorage_anims")) {
+  // Restore the contents of the text field
+  var satorage_gifs = JSON.parse(sessionStorage.getItem("satorage_gifs"));
+  var satorage_anims = JSON.parse(sessionStorage.getItem("satorage_anims"));
+  console.log("satorage_gifs = ", satorage_gifs);
+  console.log("satorage_anims = ", satorage_anims);
+  var blocks = '';
+  var k = 1;
 
-            firebase.storage().ref("animFiles").child(anim.name + ".anim").getDownloadURL().then(function(animDownloadUrl) {
-                firebase.storage().ref("mp4Files").child(anim.name + ".mp4").getDownloadURL().then(function(downloadUrl) {
-                    blocks += '<div class="box box' + k + ' fadeInUp clust">';
-                    blocks += '<div style="z-index: 111;">';
-                    blocks += '<a class="newwwww" href="javascript:;" data-name="' + anim.name + '"><i class="fa fa-plus-circle fa-2x" aria-hidden="true" ></i></a>';
-                    blocks += '<a data-url="' + animDownloadUrl + '" data-name="' + anim.name + '.anim" onclick="downloadFile(this)"><i class="fa fa-download fa-2x" aria-hidden="true"></i></a>';
-                    blocks += '<div class="animation-name">' + anim.name + '</div>';
-                    blocks += '</div>';
-                    blocks += '<video autoplay loop  muted>';
-                    blocks += '<source src="' + downloadUrl + '" type="video/mp4" />';
-                    blocks += '</video>';
-                    blocks += '</div>';
 
-                    k++;
-                    if (k === completed) {
-                        $('.zodiacCont').html(blocks);
+  for(key in satorage_gifs){
+  	console.log("key - ", key);
+  	blocks +='<div class="box box'+k+' wow fadeInUp clust">';
+        blocks +='<div>';
+        
+        blocks +='<a class="newwwww" href="#"><i class="fa fa-plus-circle fa-2x" aria-hidden="true" ></i></a>';
+        blocks +='<a href="'+satorage_anims[key]+'" download><i class="fa fa-download fa-2x" aria-hidden="true"></i></a>';
+        blocks +='</div><figure class="newFigure"> <img src="'+satorage_gifs[key]+'" class="gifPic"></figure><strong></strong>';
+        
+        blocks +='</div>';
+        ++k;
+  }
+  $('.zodiacCont').html(blocks);
 
-                        $('.newwwww').click(function() {
-                            if (firebase.auth().currentUser) {
-                                var animName = $(this).data("name");
-                                var userId = firebase.auth().currentUser.uid;
-                                firebase.database().ref("usernames").child(userId).child("mylibrary").once("value", function(snap) {
-                                    var libraryItems = snap.val();
-                                    var exists = false;
-                                    console.log(libraryItems);
-                                    libraryItems && Object.keys(libraryItems).forEach(function(itemKey) {
-                                        exists = exists || (libraryItems[itemKey] == animName);
-                                    });
-                                    if (!exists) {
-                                        var newObjRef = firebase.database().ref("usernames").child(userId).child("mylibrary").push();
-                                        newObjRef.set(animName);
-                                        alert("Added to library");
-                                    } else {
-                                        alert("Already in library");
-                                    }
-                                })
-                            } else {
-                                $('#myModal').modal('show');
-                            }
-                        })
+  $('.newwwww').click(function(){
+		var str = $(this).parent().next().find('img').attr('src');
+		
+		firebase.auth().onAuthStateChanged(function(user) {
 
-                        $.unblockUI();
-                    }
-                })
-            })
-            completed++;
-        });
-    }
+			// console.log(user.uid);
+			var newPostKey = firebase.database().ref();
+			newPostKey.push().set(str);
+		});
+	})
 }
 
-firebase.database().ref("animations").orderByChild("name").once("value", function(ss) {
-    ss = ss.val();
-    pages = ss && Math.ceil(Object.keys(ss).length / resultsPerPage);
-    var pageHTML = '<li class="active"><a href="javascript:;" onclick="getVideos(1, this);">1 <span class="sr-only">(current)</span></a></li>';
-    for (var page = 2; page <= pages; page++) {
-        pageHTML += '<li class=""><a href="javascript:;" onclick="getVideos(' + page + ', this);">' + page + ' <span class="sr-only">(current)</span></a></li>';
-    }
-    $('.repo-pages').html(pageHTML);
-});
 
-jQuery(document).ready(function() {
 
-    firebase.database().ref("/tags/").once('value').then(function(snapshot) {
 
+/*--------------------Side Bar------------------------------------*/
+/*var fireBaseSideBar = firebase.database().ref().child("tags");
+
+fireBaseSideBar.on('value',function(datasnapshot){
+	$(".sideLi").html() = datasnapshot.val();
+	})*/
+
+firebase.database().ref("/tags/").once('value').then(function(snapshot) {
         var fireObject = snapshot.val();
+        console.log("fireObject = ", fireObject);
         var t = 0;
-
-        for (var key in fireObject) {
+        for(var key in fireObject) {
+            console.log("key = ", key);
+            console.log("fireObject[",key,"] = ", fireObject[key]);
             var sabUl = "<ul class='nav nav-pills nav-stacked subMenuS'>";
-            for (var b in fireObject[key]) {
-                sabUl += "<li  class='subManuLi' data-name='" + fireObject[key][b] + "' role='presentation'><a href='javascript:;'>" + fireObject[key][b] + "</a></li>";
+            for(var b in fireObject[key]) {
+                sabUl += "<li  class = 'subManuLi' role='presentation'><a href='#'>"+fireObject[key][b]+"</a></li>";
             }
             sabUl += "</ul>";
-            var active = (t == 0) ? " class='active'" : '';
-            $(".menuS").append('<li role="presentation"' + active + '><a href="javascript:;">' + key + '</a>' + sabUl + '</li>');
+            var active = (t == 0) ? " class='active'" : ''; 
+            $(".menuS").append('<li role="presentation"'+active+'><a href="#">'+key+'</a>'+sabUl+'</li>');
             ++t;
         }
+        var subLi = '';
+        //var selected = false; //
+       // if(selected){
+        	   $(".subManuLi").off('click').on('click',function(){
 
-        $(".subManuLi").off('click').on('click', function() {
-            var subLi = '';
-            if (!$(this).hasClass("activeTag")) {
-                $(this).addClass("active activeTag");
-                var name = $(this).attr('data-name');
-                subLi += '<div class="pull-left closeDiv">';
-                subLi += '<p class="pull-left filterP">' + $(this).html() + '</p>';
-                subLi += '<button class="pull-right closeBtn" data-name="' + name + '">X</button>';
-                subLi += '</div>';
-                $(".tagName").append(subLi);
-                selected = true;
+	        	subLi += '<div class="pull-left closeDiv">';
+	        	subLi += '<p class="pull-left filterP">'+$(this).html()+'</p>';
+	        	subLi += '<button class="pull-right closeBtn">X</button>';
+	        	subLi += '</div>';
 
-            }
-            $(".closeBtn").off('click').on('click', function() {
-                var name = $(this).attr('data-name');
-                $(this).parent().remove();
-                $('.subMenuS li[data-name="' + name + '"]').removeClass('active activeTag');
-            });
+	        	$(".tagName").html(subLi);
+	            selected = true;
+	            $(".closeBtn").off('click').on('click',function(){
+	        	    $(this).parent().remove();
+	            });
+        	});
+        //}
+     	/*else{
 
-        });
+     	}*/
+
+
+        /*var storageRef = firebase.storage().ref();
+        var gsReference = firebase.storage().refFromURL('gs://animake-672fc.appspot.com');
+        var gifs = {};
+        var anims = {};*/
+    
+    /*    for (key in fireObject) {
+            var obj = fireObject[key].split("-");
+            var gif = 'gifsFires/'+obj[0];
+            var anim = 'Animake/'+obj[1];
+            var spaceRef = storageRef.child(gif);
+            storageRef.child(gif).getDownloadURL().then(function(url) {
+                var num = url.match(/F\d+/);
+                gifs[num] = url;
+                sessionStorage.setItem('satorage_gifs', JSON.stringify(gifs));
+            }).catch(function(error) {});
+    
+            storageRef.child(anim).getDownloadURL().then(function(url) {
+                var num = url.match(/F\d+/);
+                anims[num] = url;
+                sessionStorage.setItem('satorage_anims', JSON.stringify(anims));
+            }).catch(function(error) {})
+        }*/
     });
-    /*----------------------CloseButton---------------------*/
+/*----------------------CloseButton---------------------*/
+
+
+
+
 });
+
