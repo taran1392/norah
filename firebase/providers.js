cebbff33fa20c496794235a478d17d1440ccfb62
Akash Paul <iakashpaul@gmail.com>
2017-07-10 14:33:34 +0530

    Corrected double redirects

--- firebase/providers.js
+++ firebase/providers.js
@@ -1,19 +1,15 @@
-
-
-
-var uiConfig = {  
-  signInSuccessUrl: window.location.href,  
-  signInOptions: [  
-    // Указываем провайдеров для Firebase Аутентификации
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.TwitterAuthProvider.PROVIDER_ID
-  ],  
-  // Здесь вы можете специфировать условия к использованию, которые будут показаны при виджете 
-  tosUrl: '<your-tos-url>'  
-};  
+var uiConfig = {
+    /*signInSuccessUrl: window.location.href, */
+    signInOptions: [
+        // Указываем провайдеров для Firebase Аутентификации
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.TwitterAuthProvider.PROVIDER_ID
+    ],
+    // Здесь вы можете специфировать условия к использованию, которые будут показаны при виджете 
+    tosUrl: '<your-tos-url>'
+};
 // Инициализирум FirebaseUI виджет   
-var ui = new firebaseui.auth.AuthUI(firebase.auth());  
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
 // Для начала методы мы подождем пока html полностью загрузиться
-ui.start('#firebaseui-auth-container', uiConfig);  
- 
+ui.start('#firebaseui-auth-container', uiConfig);
