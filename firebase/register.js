6f2082461317607478bd0b2af0dcb47cd4e53360
Vrushali <vrushali@absentiavr.com>
2017-07-11 23:28:40 +0530

    UI correction-Firebase

--- firebase/register.js
+++ firebase/register.js
@@ -1,7 +1,17 @@
 var regEmail = document.getElementById("regEmail");
-var regPassword  = document.getElementById("regPassword");
-var regBtn  = document.getElementById("regBtn");
+var regPassword = document.getElementById("regPassword");
+var regBtn = document.getElementById("regBtn");
+var loginModalElement = $('#myModal')
+loginModalElement.on('show.bs.modal', function(event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var recipient = button.data('whatever') // Extract info from data-* attributes
+    var modal = $(this)
+    console.log(modal)
+})
 
+function closeLoginModal() {
+    loginModalElement.modal('hide');
+}
 regBtn.addEventListener('click', function(e) {
     var email = regEmail.value;
     var pass = regPassword.value;
@@ -11,17 +21,20 @@ regBtn.addEventListener('click', function(e) {
     var promise = auth.createUserWithEmailAndPassword(email, pass);
 
     promise.catch(function(error) {
-    	var errorCode = error.code;
-    	var errorMessage = error.message;
-    	alert(errorMessage /*+ " Code: " + errorCode*/);
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(errorMessage /*+ " Code: " + errorCode*/ );
     });
 
     alert('done');
+    setTimeout(Location.reload(true), 1000);
+
 });
 
-firebase.auth().onAuthStateChanged( function(firebaseUser) {
-    if(firebaseUser){
-        /*window.location = "home.html";*/
+firebase.auth().onAuthStateChanged(function(firebaseUser) {
+    if (firebaseUser) {
+        // location.reload(true);
+        closeLoginModal();
     } else {
         /*alert('Error');*/
     }
--- js/login.js
+++ js/login.js
@@ -1,17 +1,17 @@
-
 var textEmail = document.getElementById("inputEmail");
-var textPass  = document.getElementById("inputPassword");
-var btnLogIn  = document.getElementById("loginBtn");
+var textPass = document.getElementById("inputPassword");
+var btnLogIn = document.getElementById("loginBtn");
 var btnLogOut = document.getElementById("logOutBtn");
 var signUpBtn = document.getElementById("signUpBtn");
 
 var loginModalElement = $('#myModal')
-loginModalElement.on('show.bs.modal', function (event) {
+loginModalElement.on('show.bs.modal', function(event) {
     var button = $(event.relatedTarget) // Button that triggered the modal
     var recipient = button.data('whatever') // Extract info from data-* attributes
     var modal = $(this)
     console.log(modal)
 })
+
 function closeLoginModal() {
     loginModalElement.modal('hide');
 }
@@ -25,9 +25,9 @@ btnLogIn.addEventListener('click', e => {
     var promise = auth.signInWithEmailAndPassword(email, pass);
 
     promise.catch(function(error) {
-    	var errorCode = error.code;
-    	var errorMessage = error.message;
-    	alert(errorMessage + " Code: " + errorCode);
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(errorMessage + " Code: " + errorCode);
     });
 });
 
@@ -41,8 +41,8 @@ signUpBtn.addEventListener('click', e => {
 });
 
 firebase.auth().onAuthStateChanged(firebaseUser => {
-	if(!firebaseUser){
+    if (!firebaseUser) {
         window.location.href = "home.html";
-        closeLoginModal()
+        closeLoginModal();
     }
 })
